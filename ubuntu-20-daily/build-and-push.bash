0458a3dc9c810012ce4b1b2312ba8ffe1f1865ba
Sean T. Allen <sean@seantallen.com>
2020-05-14 08:41:35 -0400

    Add Ubuntu 20.04 daily driver

---
+++ ubuntu-20-daily/build-and-push.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to DockerHub when you run this ***
+#
+
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+TAG="seantallen/wsl-ubuntu-20-daily:${TODAY}"
+
+docker build --pull -t "${TAG}" "${DOCKERFILE_DIR}"
+docker push "${TAG}"
