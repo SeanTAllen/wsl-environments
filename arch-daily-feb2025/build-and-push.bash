1968f532d57ccde0b9eed7d81bf8e37c701beb7d
Sean T. Allen <sean@seantallen.com>
2025-02-15 21:04:40 +0000

    Add an arch based daily driver environment

---
+++ arch-daily-feb2025/build-and-push.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+DOCKERFILE_DIR="$(dirname "$0")"
+NAME="ghcr.io/seantallen/wsl-environments-arch-daily-feb2025"
+
+# with date as tag
+TAG_AS=$(date +%Y%m%d)
+docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TAG_AS}"
+
+# with latest as tag
+TAG_AS="latest"
+docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${NAME}:${TAG_AS}"
