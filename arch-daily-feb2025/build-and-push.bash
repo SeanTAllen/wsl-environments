f1ff7a95ed1a9bd31f30cfaa60a5231a9e7b85b3
Sean T. Allen <sean@seantallen.com>
2026-01-02 22:06:03 -0500

    Add roboto mono font as part of default set up in archlinux env

--- arch-daily-feb2025/build-and-push.bash
+++ arch-daily-feb2025/build-and-push.bash
@@ -8,10 +8,10 @@ NAME="ghcr.io/seantallen/wsl-environments-arch-daily-feb2025"
 
 # with date as tag
 TAG_AS=$(date +%Y%m%d)
-docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker build --pull -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
 docker push "${NAME}:${TAG_AS}"
 
 # with latest as tag
 TAG_AS="latest"
-docker build -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker build --pull -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
 docker push "${NAME}:${TAG_AS}"
