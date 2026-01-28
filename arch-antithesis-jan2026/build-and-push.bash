82caaab704a4bfad673d567381e967bab32ec9bb
Sean T. Allen <sean@seantallen.com>
2026-01-27 23:57:50 -0500

    Less work building antithesis arch environment

--- arch-antithesis-jan2026/build-and-push.bash
+++ arch-antithesis-jan2026/build-and-push.bash
@@ -6,12 +6,7 @@ set -o nounset
 DOCKERFILE_DIR="$(dirname "$0")"
 NAME="ghcr.io/seantallen/wsl-environments-arch-antithesis-jan2026"
 
-# with date as tag
-TAG_AS=$(date +%Y%m%d)
-docker build --pull -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
-docker push "${NAME}:${TAG_AS}"
-
-# with latest as tag
-TAG_AS="latest"
-docker build --pull -t "${NAME}:${TAG_AS}" "${DOCKERFILE_DIR}"
-docker push "${NAME}:${TAG_AS}"
+TODAY=$(date +%Y%m%d)
+docker build --pull -t "${NAME}:${TODAY}" -t "${NAME}:latest" \
+  "${DOCKERFILE_DIR}"
+docker push "${NAME}" --all-tags
